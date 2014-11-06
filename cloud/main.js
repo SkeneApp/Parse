ce1f5220aa4128da3dd3b973499274ccb94ea333
Roman Zaynetdinov <romanznet@gmail.com>
2014-11-06 18:23:43 +0200

    Implement getMapData function

--- cloud/main.js
+++ cloud/main.js
@@ -31,14 +31,13 @@ Parse.Cloud.define("hello", function(request, response) {
 Parse.Cloud.define('get', function (req, res) {
   var query = new Parse.Query('message');
 
-  var rad = req.params.radius;
-  var lat = req.params.latitude;
-  var lon = req.params.longitude;
+  var rad = +req.params.radius;
+  var lat = +req.params.latitude;
+  var lon = +req.params.longitude;
   var p_id = req.params.parentId;
   var count = +req.params.count || 50;
   var R = 6371; // Earth's radius
   var edges = {};
-
 
   if(rad & lat & lon) {
     // Select by location
@@ -48,16 +47,6 @@ Parse.Cloud.define('get', function (req, res) {
     edges.min_lat = lat - (rad / R).toDegrees();
     edges.max_lon = lon + ( rad / R / Math.cos(lat.toRadians()) ).toDegrees();
     edges.min_lon = lon - ( rad / R / Math.cos(lat.toRadians()) ).toDegrees();
-
-    /**
-     * array('parent_id = ' => 0,
-            'latitude >='  => $min_lat,
-            'latitude <='  => $max_lat,
-            'longitude >=' => $min_long,
-            'longitude <=' => $max_long,
-            'pubTime >='   => $timestamp,
-            'pubTime <='   => $current_time),
-     */
 
     query.greaterThanOrEqualTo('latitude', edges.min_lat);
     query.lessThanOrEqualTo('latitude', edges.max_lat);
@@ -69,7 +58,6 @@ Parse.Cloud.define('get', function (req, res) {
     query.equalTo('parentId', p_id);
   } else {
     // Select all
-
   }
 
   query.limit(50);
@@ -79,7 +67,39 @@ Parse.Cloud.define('get', function (req, res) {
       res.success(results);
     },
     error : function () {
-      res.error("Error");
+      res.error('Error');
+    }
+  })
+});
+
+/**
+ * GET map data
+ * Available parameters:
+ *   min_lat: min/max lat/long of message
+ *   max_lat: --
+ *   min_lon: --
+ *   max_lon: --
+ *   timestamp (default: 0): get messages posted after this time. Format: Unix timestamp
+ *   count (default: 50): maximum number of messages to return
+ */
+Parse.Cloud.define('map_data', function (req, res) {
+  var count = +req.params.count || 50;
+  var query = new Parse.Query('message');
+
+  query.greaterThanOrEqualTo('latitude', +req.params.min_lat);
+  query.lessThanOrEqualTo('latitude', +req.params.max_lat);
+  query.greaterThanOrEqualTo('longitude', +req.params.min_lon);
+  query.lessThanOrEqualTo('latitude', +req.params.max_lon);
+  query.equalTo('parentId', 0);
+
+  query.limit(count);
+  query.descending('objectId');
+  query.find({
+    success : function (results) {
+      res.success(results);
+    },
+    error : function () {
+      res.error(arguments);
     }
   })
 });

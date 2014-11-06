d0bb4cb35cddf8317b8bd5f1bf8850f324006ff3
Roman Zaynetdinov <romanznet@gmail.com>
2014-11-06 23:53:43 +0200

    Remove reserved keyword

--- cloud/main.js
+++ cloud/main.js
@@ -88,7 +88,7 @@ Parse.Cloud.define('map_data', function (req, res) {
 
   query.limit(count);
   query.descending('objectId');
-  query.select('latitude', 'longitude', 'createdAt');
+  query.select('latitude', 'longitude');
   query.find({
     success : function (results) {
       res.success(results);
